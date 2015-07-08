9b8eeded769d694af89aedef4c5f29bc03701532
Amy Burka <aburka@gmail.com>
2015-07-08 12:59:23 -0400

    implement user gallery

--- app/routes/users/current.js
+++ app/routes/users/current.js
@@ -3,6 +3,18 @@ import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixi
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model: function(){
-    return this.get('session.currentUser');
-  }
+    return Ember.$.ajax({
+      url: 'https://api.artsy.net/api/artworks?gene_id=4dd689edb084a900010015c6',
+      type: 'GET',
+      dataType: 'json',
+      contentType: "application/json",
+      headers: {
+        'X-Xapp-Token': "JvTPWe4WsQO-xqX6Bts49ijmR_KEI0T62dtoOGQiNeTqbi7W5EbiSyH6A_gXh_NlF2ME2IFxOQ22OBPuxLKBKWSFmMyGq_AWtN7prEmzILl6mrBc333kurt2TJAP9DA4F29C_uJ2yetHw1bEDQPdfiRhuDKAI5AnYxnXneI4EcohSADbj5Oayu7uSzh8nqy_0StlZhc2usXU56dJPx_y_0XJc6qajxoi8JEE0tFfkiE="
+      },
+
+      success: function(data){
+          console.log("success:artwork", data);
+      }
+      });
+    },
 });
