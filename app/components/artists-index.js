a4b0af298ceaccbde49a52c095b6469f9b5ed658
Amy Burka <aburka@gmail.com>
2015-07-08 14:25:29 -0400

    add 'like' buttons to artwork and artist page if user is authenticated

--- app/components/artists-index.js
+++ app/components/artists-index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    like: function(){
+      console.log();
+    }
+  }
+
 });
--- app/components/artwork-index.js
+++ app/components/artwork-index.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    like: function(){
+      console.log();
+    }
+  }
 });
