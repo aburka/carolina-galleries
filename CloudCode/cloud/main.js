c806756111e50b532afecbe1a6688089f98b214c
Amy Burka <aburka@gmail.com>
2015-07-09 15:01:07 -0400

    add likes to user and display user's likes on user/current page

--- CloudCode/cloud/main.js
+++ CloudCode/cloud/main.js
@@ -6,9 +6,6 @@ Parse.Cloud.define("like", function(request, response) {
   var query = new Parse.Query("Artwork");
   query.equalTo('id', 'artwork.id');
     user.addUnique("Artwork", request.params.artwork).save();
-//  } else {
-//    console.log('nice try, bucko');
-//  }
 
   response.success({
     artwork: request.params.artwork,
