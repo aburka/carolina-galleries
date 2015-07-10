967a787c94506bda4e45010af3570cf33eda540e
Amy Burka <aburka@gmail.com>
2015-07-10 14:22:18 -0400

    move gallery contact to component

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(){
     return this.get('session.currentUser');
+  },
+
+  actions: {
+    submitContact: function(){
+      console.log(this.get('model'));
+    }
   }
 });
