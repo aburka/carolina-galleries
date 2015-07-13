ad91dff7e271b38f98aaaa928d68387523982ea3
Amy Burka <aburka@gmail.com>
2015-07-13 09:51:44 -0400

    Added computed property to artwork-show to return large version of the selected image

--- app/components/artwork-show.js
+++ app/components/artwork-show.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-});
+    //gt thumbnail url use regular expression to remove anything after slash, add large
+    imageUrl: function(){
+      var curies = this.get('this.attrs.model.value._links.curies');
+      var url = curies.objectAt(0).href;
+      return url.replace('{rel}', 'large.jpg');
+    }.property('imageUrl')
+  });
