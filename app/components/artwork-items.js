d0141f69cfa59c32c9c949d498f8166feef0e6ec
Amy Burka <aburka@gmail.com>
2015-07-15 10:32:08 -0400

    swap like/unlike on click

--- app/components/artwork-items.js
+++ app/components/artwork-items.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isLiked: false,
 
   actions: {
     like: function(artwork) {
       this.sendAction('like', artwork);
+      this.set('isLiked', true);
     },
     unlike: function(artwork){
       this.sendAction('unlike', artwork);
+      this.set('isLiked', false);
     }
   },
 
