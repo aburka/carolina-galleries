d75a81e5286866fc87f3f920e3862a70f8ca841c
Amy Burka <aburka@gmail.com>
2015-07-15 08:46:38 -0400

    associate user's name with uploaded image in community gallery

--- app/components/users-gallery-create.js
+++ app/components/users-gallery-create.js
@@ -1,11 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  username: function(){
+    return this.get('session.currentUser.username');
+  }.property('username'),
+
+  name: function(){
+    var firstName = this.get('session.currentUser.firstName');
+    var lastName = this.get('session.currentUser.lastName');
+    return firstName + " " + lastName;
+  }.property('name'),
+
   actions: {
     uploadImage: function(){
-      this.sendAction('uploadImage', this.getProperties('url', 'title'));
+      this.sendAction('uploadImage', this.getProperties('url', 'title', 'user', 'name'));
       this.set('url');
       this.set('title');
+      this.set('username');
+      this.set('name');
     }
   }
 });
