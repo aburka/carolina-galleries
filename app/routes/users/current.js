4c821beb7fb821cb4e49f8c374b5d7c8caeb361c
Amy Burka <aburka@gmail.com>
2015-07-15 13:23:39 -0400

    removed unnecessary model

--- app/routes/users/current.js
+++ app/routes/users/current.js
@@ -2,21 +2,6 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model: function(){
-    return Ember.$.ajax({
-      url: 'https://api.artsy.net/api/artworks?gene_id=4dd689edb084a900010015c6',
-      type: 'GET',
-      dataType: 'json',
-      contentType: "application/json",
-      headers: {
-        'X-Xapp-Token': "JvTPWe4WsQO-xqX6Bts49nNq_yb04yJEZ-XTnaQhTOb0wwNLJnpQOFek7FPtmQ1qWNo3U0dyRdAWsXWQMLB8PL-f7TX5pajZfIEHjJ5ddvoObRiLuDTPCne2LdqzesvNTT5pbmXsrcSTe9o5pcR5yvMYiXchQJkatty9JVT2x5Zy9qmcxw_Y7BafmJdUu2LY2eyf0GGwbWaFth0fgk4doD_QaVGE1czcsRrovIKtqZ4="
-      },
-
-      success: function(data){
-          console.log("success:artwork", data);
-      }
-      });
-    },
   actions: {
     unlike: function(artwork) {
       var adapter = this.store.adapterFor('application');
