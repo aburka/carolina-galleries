acd8c754467f6d25adab85755c1534a5d1fd7af8
Amy Burka <aburka@gmail.com>
2015-07-20 11:57:51 -0400

    re-implement new user signup component and sessions create component for mobile, smooth out mobile styles

--- app/components/sessions-create.js
+++ app/components/sessions-create.js
@@ -3,18 +3,8 @@ import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
 export default Ember.Component.extend(LoginControllerMixin,{
   authenticator: 'authenticator:parse-username',
-  isHidden: true,
 
   tagName: 'span',
 
   classNames:['user-login-create'],
-
-  actions: {
-    show: function(){
-      this.set('isHidden', false);
-    },
-    cancel: function(){
-      this.set('isHidden', true);
-    }
-  }
 });
