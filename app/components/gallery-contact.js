e562995bec182aeca367bee1eb621737d1292052
Amy Burka <aburka@gmail.com>
2015-07-21 23:03:30 -0400

    added hours of operation and message that changes if open/not open

--- app/components/gallery-contact.js
+++ app/components/gallery-contact.js
@@ -1,9 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // isOpen: false,
+
   actions: {
     submitContact: function(){
       this.sendAction('action', this.get('model'));
+    }
+  },
+
+  open: function(){
+    //get current time
+    var now = new Date();
+    //get day of week
+    var weekDay = now.getDay();
+    // get current hour
+    var hour = now.getHours();
+
+    if(weekDay > 3 && weekDay < 6 && hour > 11 && hour < 19){
+      this.set('isOpen', true);
+    } else {
+      this.set('isOpen', false);
     }
   }
 });
