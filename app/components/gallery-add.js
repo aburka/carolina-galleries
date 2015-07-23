5a67a7faf39e72e8bed46cd9290f04ba017963f1
Amy Burka <aburka@gmail.com>
2015-07-22 20:23:40 -0400

    move chat to community gallery and add styles. Also add styles from jquery share plugin

--- app/components/gallery-add.js
+++ app/components/gallery-add.js
@@ -5,11 +5,19 @@ export default Ember.Component.extend({
 
   classNames: ['gallery'],
 
+  isHidden: true,
+
   actions: {
     filepick: function(){
       filepicker.pick(function(blob){
         this.sendAction('filepick', blob);
       }.bind(this));
+    },
+    openChat: function(){
+      this.set('isHidden', false);
+    },
+    closeChat: function(){
+      this.set('isHidden', true);
     }
   }
 });
